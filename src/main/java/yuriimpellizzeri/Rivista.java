e0b06dd469868a0366f5afdc502a829df27ec909
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-08-02 10:21:54 +0200

    Created Rivista Class

---
+++ src/main/java/yuriimpellizzeri/Rivista.java
@@ -0,0 +1,26 @@
+package yuriimpellizzeri;
+
+public class Rivista extends Catalogo {
+    private Periodicita periodicita;
+
+
+    public Rivista(String isbn, String titolo, int annoPubblicazione, int numeroPagine, Periodicita periodicita) {
+        super(isbn, titolo, annoPubblicazione, numeroPagine);
+        this.periodicita = periodicita;
+    }
+
+    public Periodicita getPeriodicita() {
+        return periodicita;
+    }
+
+    public void setPeriodicita(Periodicita periodicita) {
+        this.periodicita = periodicita;
+    }
+
+    @Override
+    public String toString() {
+        return "Rivista{" +
+                "periodicita=" + periodicita +
+                '}';
+    }
+}
