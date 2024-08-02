5409950412955750446a43c713c496d3efd58dc7
Yuri Impellizzeri <128586274+yvr1x02@users.noreply.github.com>
2024-08-02 09:51:45 +0200

    Created Catalogo Class

---
+++ src/main/java/yuriimpellizzeri/Catalogo.java
@@ -0,0 +1,59 @@
+package yuriimpellizzeri;
+
+
+public abstract class Catalogo {
+    private String isbn;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+
+    public Catalogo(String isbn, String titolo, int annoPubblicazione, int numeroPagine) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalogo{" +
+                "isbn='" + isbn + '\'' +
+                ", titolo='" + titolo + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", numeroPagine=" + numeroPagine +
+                '}';
+    }
+}
+
